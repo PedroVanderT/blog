731662395e4c6bca2bc55e60f86b000f7ec0050a
Cristopher V <pedrocristopherv@gmail.com>
2023-06-20 21:03:13 -0400

    Modelo Creado

--- index.py
+++ index.py
@@ -1,5 +1,19 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 datos = pd.read_csv("ingreso.csv")
+regresion = linear_model.LinearRegression()
+horas = datos["horas"].values.reshape((-1,1))
+modelo = regresion.fit(horas, datos["ingreso"])
+print("Interseccion (b)", modelo.intercept_)
+print("pendiente (m)", modelo.coef_)
+entrada = [[39],[40],[43],[44]]
+p = modelo.predict(entrada)
+print(p)
+plt.scatter(entrada, modelo.predict(entrada), color="red")
+plt.plot(entrada, modelo.predict(entrada), color="yellow")
+plt.ylabel("Ingreso ($)")
+plt.xlabel("Promedio de horas semanales trabajdas")
+plt.scatter(datos["horas"], datos["ingreso"], color="blue")
+plt.show()
